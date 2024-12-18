89955d628076e044f257badd03889300d614b605
kmurphy <kmurphy@wit.ie>
2024-12-18 13:15:06 +0000

    added scripts and input data

---
+++ mc_clusters_to_tutors.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+
+import os, shutil, glob
+import yaml
+import pandas as pd
+import argparse
+
+
+def clean_filename(s):
+    return (s.replace(" ","_").replace("&","and")
+            .replace(",","").replace(".","")
+            .replace("(","").replace(")","")
+    )
+
+
+def load(folder='descriptors', id="*"):
+    files = sorted(glob.glob(f"module_catalogue/{folder}/yaml/{id}.yaml"))
+
+    return {file.rsplit("/")[-1].split(".")[0]:yaml.safe_load(open(file)) for file in files}
+
+
+modules = load(folder="modules")
+descriptors = load(folder="descriptors")
+programmes = load(folder="programmes")
+schedules = load(folder="schedules")
+
+topic_content = """# {title}
+
+{description}
+"""
+
+unit_content = """# {title}
+
+{description}
+"""
+
+module_content = """---
+icon:
+  type: {icon}
+  color: {color}
+---
+
+{description}
+"""
+
+# The following CSV stores the icons/colors for each module 
+# Columns Icon/Color were populated by Pete/chatGPT
+df = pd.read_csv("module_display_settings.csv")
+
+OUTPUT = "."
+
+def run(verbose=False):
+
+    clusters = {}
+    for ref,module in modules.items():
+        group, cluster = module['group'], module['subgroup']
+        if group not in clusters: clusters[group] = {}
+        if cluster not in clusters[group]: clusters[group][cluster] = []
+        clusters[group][cluster].append((module['ref'],module['name']))    
+
+    clusters = dict(sorted(clusters.items()))
+    for group in clusters:
+        clusters[group] = dict(sorted(clusters[group].items()))
+        for cluster in clusters[group]:
+            clusters[group][cluster] = sorted(clusters[group][cluster], key=lambda x: x[1])
+                    
+    # group <-> topic
+    for k, group in enumerate(clusters,1):
+
+        topic = f"{OUTPUT}/topic-{k:02d}-{clean_filename(group)}"
+        os.makedirs(topic, exist_ok=True)
+        content = topic_content.format(title=group, description="")
+        open(f"{topic}/topic.md", "wt").write(content)
+
+        # cluster <-> unit
+        for kk, cluster in enumerate(clusters[group],1):
+
+            unit = f"unit-{kk}-{clean_filename(cluster)}"
+            if verbose: print(f"\t{unit}")
+            os.makedirs(f"{topic}/{unit}", exist_ok=True)
+            title = f"{cluster}"
+            content = unit_content.format(title=title, description="")
+            open(f"{topic}/{unit}/topic.md", "wt").write(content)
+
+            
+            # module <-> talk
+            for kkk, (ref,title) in enumerate(clusters[group][cluster],1):
+                
+                target = f"talk-{kk:02d}-" + clean_filename(title)
+                card = f"talk-{kk:02d}-{target}"
+                if verbose: print(f"\t\t{card}")
+                os.makedirs(f"{topic}/{unit}/{card}", exist_ok=True)
+
+                df_tmp = df.query(f"Module_Title=='{title}'")
+                if df_tmp.shape[0]:
+                    icon = df_tmp.Icon.values[0]
+                    color = df_tmp.Color.values[0]
+                else:
+                    icon = 'fa:desktop'
+                    color = 398126
+                description = descriptors[ref]['aim'][:150] + " ... " 
+                content = module_content.format(title=title, icon=icon, color=color, description=description)
+                open(f"{topic}/{unit}/{card}/{target}" + ".md", "wt").write(content)
+
+                source = f"module_catalogue/descriptors/pdf/{ref}.pdf"
+                shutil.copyfile(source, f"{topic}/{unit}/{card}/{target}" + ".pdf")
+
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='Generate a tutor course for module clusters in the module catalogue.')
+                                    
+    parser.add_argument('-v','--verbose', action="store_true", default=False, help='Verbose output')
+
+args = parser.parse_args()
+run(verbose=args.verbose)
---
+++ mc_programmes_to_tutors.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+
+import os, shutil, glob
+import yaml
+import pandas as pd
+import argparse
+
+
+def clean_filename(s):
+    return (s.replace(" ","_").replace("&","and")
+            .replace(",","").replace(".","")
+            .replace("(","").replace(")","")
+    )
+
+
+def load(folder='descriptors', id="*"):
+    files = sorted(glob.glob(f"module_catalogue/{folder}/yaml/{id}.yaml"))
+
+    return {file.rsplit("/")[-1].split(".")[0]:yaml.safe_load(open(file)) for file in files}
+
+
+modules = load(folder="modules")
+descriptors = load(folder="descriptors")
+programmes = load(folder="programmes")
+schedules = load(folder="schedules")
+
+topic_content = """# {title}
+
+Programme leader: {leader}
+"""
+
+unit_content = """# {title}
+
+{description}
+"""
+
+module_content = """---
+icon:
+  type: {icon}
+  color: {color}
+---
+
+{description}
+"""
+
+# The following CSV stores the icons/colors for each module 
+# Columns Icon/Color were populated by Pete/chatGPT
+df = pd.read_csv("module_display_settings.csv")
+
+OUTPUT = "."
+
+def run(programme_codes, verbose=False):
+
+    # programme <-> topic
+    for k, code in enumerate(programme_codes):
+
+        programme = programmes[code]
+        schedule = schedules[code]
+
+        topic = f"{OUTPUT}/topic-{k:02d}-{code}"
+        if verbose: print(f"{topic}")
+        os.makedirs(topic, exist_ok=True)
+        content = topic_content.format(title=programme['full title'], leader=programme['leader'])
+        open(f"{topic}/topic.md", "wt").write(content)
+
+        # semester <-> unit
+        for semester, semester_modules in schedule['semesters'].items():
+
+            unit = f"unit-{semester}" 
+            if verbose: print(f"\t{unit}")
+            os.makedirs(f"{topic}/{unit}", exist_ok=True)
+            title = f"Semester {semester}"
+            content = unit_content.format(title=title, description="TODO (semster)")
+            open(f"{topic}/{unit}/topic.md", "wt").write(content)
+
+            # module <-> talk
+            for kk, module in enumerate(semester_modules['mandatory'] + semester_modules['elective'],1):  
+                ref = module['code']
+                title = module['name']
+
+                # TODO if we want to jump to cluster view  
+                # group = modules[ref]['group']
+                # subgroup = modules[ref]['subgroup']
+
+                target = f"talk-{kk:02d}-" + clean_filename(title)
+                card = f"talk-{kk:02d}-{target}"
+                if verbose: print(f"\t\t{card}")
+                os.makedirs(f"{topic}/{unit}/{card}", exist_ok=True)
+
+                df_tmp = df.query(f"Module_Title=='{title}'")
+                if df_tmp.shape[0]:
+                    icon = df_tmp.Icon.values[0]
+                    color = df_tmp.Color.values[0]
+                else:
+                    icon = 'fa:desktop'
+                    color = 398126
+                description = descriptors[ref]['aim'][:150] + " ... " 
+                content = module_content.format(title=title, icon=icon, color=color, description=description)
+                open(f"{topic}/{unit}/{card}/{target}" + ".md", "wt").write(content)
+
+                source = f"module_catalogue/descriptors/pdf/{ref}.pdf"
+                shutil.copyfile(source, f"{topic}/{unit}/{card}/{target}" + ".pdf")
+
+
+CM_PROGRAMMES = "WD_KINFT_D WD_KINTE_B WD_KCRCO_B WD_KCOMC_D WD_KDEVP_B WD_KCOFO_B WD_KCMSC_B WD_KCOSC_G WD_KDAAN_G WD_KCESS_R WD_KISYP_R".split()
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='Generate a tutor course for a set of programmes in the module catalogue.')
+                                    
+    parser.add_argument('-v','--verbose', action="store_true", default=False, help='Verbose output')
+
+    # TODO Need to talk to EdeL about numbering in tutors
+    parser.add_argument('programme_codes', action="store", nargs='*', default=CM_PROGRAMMES)
+
+args = parser.parse_args()
+run(args.programme_codes, verbose=args.verbose)
